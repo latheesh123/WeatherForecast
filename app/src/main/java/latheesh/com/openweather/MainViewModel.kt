aba9f9d9ea672a4094675ff767be4d7a56d35936
Latheesh <latheeshv90@gmail.com>
2020-01-23 13:29:42 -0600

    retrofit

--- app/src/main/java/latheesh/com/openweather/MainViewModel.kt
+++ app/src/main/java/latheesh/com/openweather/MainViewModel.kt
@@ -1,14 +1,8 @@
 package latheesh.com.openweather
 
-import android.content.Context
-import android.telecom.Call
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import latheesh.com.openweather.model.WeatherResponse
 import latheesh.com.openweather.service.WeatherRetrofit
 import retrofit2.Callback
@@ -18,7 +12,6 @@ class MainViewModel : ViewModel() {
 
     var _weatherResponse = MutableLiveData<WeatherResponse>()
     var weatherResponse: LiveData<WeatherResponse> = _weatherResponse
-    val viewModelScope = CoroutineScope(Dispatchers.IO)
     fun getDetails(cityname: String) {
         //api call
         val response = WeatherRetrofit.getWeatherService()
